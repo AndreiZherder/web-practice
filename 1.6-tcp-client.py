3fb248eaad71d0b100da5efb473dbf15e0683463
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2022-02-28 22:01:40 +0300

    simple-tcp-client

---
+++ 1.6-tcp-client.py
@@ -0,0 +1,24 @@
+import socket
+
+
+def main():
+    with socket.socket() as conn:
+        address = ('185.187.91.100', 2222)
+        conn.connect(address)
+        print('Connected to {}:{}'.format(address[0], address[1]))
+        while True:
+            try:
+                data = input('Enter data: ').encode()
+                conn.sendall(data)
+                data = conn.recv(1024)
+                if not data:
+                    print('Connection closed by server')
+                    break
+                print(data.decode())
+            except KeyboardInterrupt:
+                print('Closed by user')
+                return
+
+
+if __name__ == '__main__':
+    main()
