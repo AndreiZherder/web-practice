d161c1940fa1528df430089976402af6ada7da2a
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2022-02-28 22:01:20 +0300

    simple-echo-server fix

--- 1.6-tcp-server.py
+++ 1.6-tcp-server.py
@@ -2,25 +2,37 @@
 
 import socket
 
-with socket.socket() as server:
-    server.bind(('', 2222))
-    server.listen(1)
 
-    while True:
-        conn, address = server.accept()
-        with conn:
-            print('Connected by {}'.format(address))
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    break
-                else:
-                    if data == b'close':
-                        conn.close()
-                        print('Connection closed')
+def main():
+    with socket.socket() as server:
+        server.bind(('', 2222))
+        server.listen(1)
+
+        while True:
+            try:
+                conn, address = server.accept()
+            except KeyboardInterrupt:
+                print('Closed by user')
+                return
+            with conn:
+                print('Connected by {}'.format(address))
+                while True:
+                    try:
+                        data = conn.recv(1024)
+                    except KeyboardInterrupt:
+                        print('Closed by user')
+                        return
+                    if not data:
+                        print('Connection closed by client {}'.format(address))
                         break
                     else:
-                        print(data.decode())
-                        conn.sendall(data)
+                        if data == b'close':
+                            print('Connection closed due to close command from {}'.format(address))
+                            break
+                        else:
+                            print(data.decode())
+                            conn.sendall(data)
 
 
+if __name__ == '__main__':
+    main()
