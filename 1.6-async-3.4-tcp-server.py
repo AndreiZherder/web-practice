0b28c6d8e6d3fe4acd336d0a435e60be7df3bcb7
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2022-04-06 12:05:25 +0300

    fix

--- 1.6-async-3.4-tcp-server.py
+++ 1.6-async-3.4-tcp-server.py
@@ -19,17 +19,7 @@ def handle_request(request: bytes) -> bytes:
     return request.decode().upper().encode()
 
 
-@asyncio.coroutine
-def main():
-    server = yield from asyncio.start_server(serve_client, port=2222)
-    try:
-        yield from server.serve_forever()
-    finally:
-        server.close()
-        yield from server.wait_closed()
-
-
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
-    loop.close()
+    loop.create_task(asyncio.start_server(serve_client, port=2222))
+    loop.run_forever()
